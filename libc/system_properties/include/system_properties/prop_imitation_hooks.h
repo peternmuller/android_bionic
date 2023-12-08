d552ff0f37b958d7e126b60b470774b86df27545
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-12-08 05:53:08 +0530

    system_properties: Introduce native PropImitationHooks
    Allows spoofing first API and security patch level in order to pass
    SafetyNet CTS/Play Integrity on build fingerprints newer than 2018.
    
    Inspired by https://github.com/chiteroman/PlayIntegrityFix
    
    Squashed:
    
    Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Date:   Wed Oct 30 22:46:25 2024 +0530
    
        PIHooks: Allow disabling property spoofing [2/2]
    
        Change-Id: Ifb7e4304012225792aa5fd502a1d092fa9136d97
    
    Change-Id: If4dd24abe84edcf5e98d27fb5f78ee99f266b4bd

---
+++ libc/system_properties/include/system_properties/prop_imitation_hooks.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2023 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <async_safe/log.h>
+#include "system_properties.h"
+
+#define DEBUG false
+#define LOG_TAG "PropImitationHooks/Native"
+#define PIH_LOG(fmt, ...) if (DEBUG) \
+  async_safe_format_log(ANDROID_LOG_INFO, LOG_TAG, "%s: " fmt, __func__, ##__VA_ARGS__)
+
+class PropImitationHooks {
+ public:
+  PropImitationHooks() = default;
+  void OnFind(SystemProperties *props, const char** name);
+
+ private:
+  bool IsEnabled(SystemProperties *props);
+};
---
+++ libc/system_properties/prop_imitation_hooks.cpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2023 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "system_properties/prop_imitation_hooks.h"
+
+#define GMS_UNSTABLE "com.google.android.gms.unstable"
+#define PROP_ENABLED "persist.sys.pihooks.enabled_features"
+#define FEATURE_GMS_PROP_IMITATION (1 << 0)
+
+#define PROP_SECURITY_PATCH "ro.build.version.security_patch"
+#define PROP_PIH_SECURITY_PATCH "persist.sys.pihooks.security_patch"
+
+#define PROP_FIRST_API_LEVEL "ro.product.first_api_level"
+#define PROP_PIH_FIRST_API_LEVEL "persist.sys.pihooks.first_api_level"
+
+void PropImitationHooks::OnFind(SystemProperties *props, const char** name) {
+  if (getprogname() == nullptr || strcmp(getprogname(), GMS_UNSTABLE) != 0) {
+    return;
+  }
+  if (!strcmp(*name, PROP_ENABLED) || !IsEnabled(props)) {
+    return;
+  }
+  PIH_LOG("name is %s", *name);
+  if (strcmp(*name, PROP_SECURITY_PATCH) == 0) {
+    *name = PROP_PIH_SECURITY_PATCH;
+  } else if (strcmp(*name, PROP_FIRST_API_LEVEL) == 0) {
+    *name = PROP_PIH_FIRST_API_LEVEL;
+  } else {
+    return;
+  }
+  PIH_LOG("name changed to %s", *name);
+}
+
+bool PropImitationHooks::IsEnabled(SystemProperties *props) {
+  char value[PROP_VALUE_MAX];
+  props->Get(PROP_ENABLED, value);
+  return (value[0] == '\0' || (atoi(value) & FEATURE_GMS_PROP_IMITATION) != 0);
+}
