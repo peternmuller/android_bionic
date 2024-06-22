5f258705d77ec7de51b1c852513aed40608813ed
Alvin Francis <nivlafx@gmail.com>
2024-06-22 01:16:03 -0400

    system_properties: Only spoof the initial sdk to pass basic
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- libc/system_properties/prop_imitation_hooks.cpp
+++ libc/system_properties/prop_imitation_hooks.cpp
@@ -13,9 +13,6 @@
 #define PROP_ENABLED "persist.sys.pihooks.enabled_features"
 #define FEATURE_GMS_PROP_IMITATION (1 << 0)
 
-#define PROP_SECURITY_PATCH "ro.build.version.security_patch"
-#define PROP_PIH_SECURITY_PATCH "persist.sys.pihooks.security_patch"
-
 #define PROP_FIRST_API_LEVEL "ro.product.first_api_level"
 #define PROP_PIH_FIRST_API_LEVEL "persist.sys.pihooks.first_api_level"
 
@@ -27,9 +24,7 @@ void PropImitationHooks::OnFind(SystemProperties *props, const char** name) {
     return;
   }
   PIH_LOG("name is %s", *name);
-  if (strcmp(*name, PROP_SECURITY_PATCH) == 0) {
-    *name = PROP_PIH_SECURITY_PATCH;
-  } else if (strcmp(*name, PROP_FIRST_API_LEVEL) == 0) {
+  if (strcmp(*name, PROP_FIRST_API_LEVEL) == 0) {
     *name = PROP_PIH_FIRST_API_LEVEL;
   } else {
     return;
